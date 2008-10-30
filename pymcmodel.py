11c26905a0055926013b5378e45eed0ad5623c47
Alexandra (Tally) Balaban <abalaban@live.unc.edu>
2008-10-30 21:42:22 +0000

    pymc updated with new priors/proposal distributions

--- pymcmodel.py
+++ pymcmodel.py
@@ -1,11 +1,42 @@
-import pymc as py
-import numpy as nu
+import pymc as pymc
+import numpy as numpy
+import random as random
 
 def MetropolisHastings(G, delta, sigma):
     #First sample (sigma*, G*) from Q(. | sigma, G)
+    x = random()
+    lambdaStar = x.gammavariate(.3,.5)
+    betaStar = x.gammavariate(.3,.5)
+    BStar = x.gammavariate(.3.5)
+    alphaStar = x.gammavariate(.3,.5)
+    muStar = x.gammavariate(.3,.5)
+    
+    sigmaStar= [randomLambda, randomBeta, randomB, randomalpha, randomMu]
+    
+    probBetaStar=CalculateGamma(lambdaStar,.3,.5)
+    probLambdaStar=CalculateGamma(betaStar,.3,.5)
+    probBStar=CalculateGamma(BStar,.3,.5)
+    probAlphaStar=CalculateGamma(alphaStar,.3,.5)
+    probMuStar=CalculateGamma(muStar,.3,.5)
+
+    #wStarOfSigma is the probability of sigma star under the prior distribution, W    
+    wStarOfSigma = probLambda*probBeta*probB*probAlpha*probMu
+
+    lam = sigma[0]
+    beta = sigma[1]
+    B = sigma[2]
+    alpha = sigma[3]
+    mu = sigma[4]
+    probBeta=CalculateGamma(lam,.3,.5)
+    probLambda=CalculateGamma(beta,.3,.5)
+    probB=CalculateGamma(B,.3,.5)
+    probAlpha=CalculateGamma(alpha,.3,.5)
+    probMu=CalculateGamma(mu,.3,.5)
+    wOfSigma = probBeta*probLambda*probB*probAlpha*probMu    
+    
     S = MCMC(G, db='ram')
     S.sample(1000, length=None, verbose=0)
-    GStar = uniform(S)
+    ran = uniform(1)
     #proposal_sig = P(G*, delta | sigma) * W(sigma*)
     #              ------------------------------
     #               P(G, delta| sigma) * W(sigma) 
