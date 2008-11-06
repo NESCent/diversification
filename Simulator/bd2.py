3f712446566549d381766d0c609d2a14c5b8790b
Alexandra (Tally) Balaban <abalaban@live.unc.edu>
2008-11-06 18:53:19 +0000

    working on bd2

--- Simulator/bd2.py
+++ Simulator/bd2.py
@@ -12,6 +12,8 @@ import random
 # a list with the first component being the migration time, and the second component the
 # state to which the lineage migrated.
 # Assume there is at first one lineage, lineage 0.
+# Assume a lineage in state 0 has the same rate of migration to state 0 as a lineage in state 1
+# Assume a lineage in state 1 has the same rate of migration to state 1 as a lineage in state 0
 
 
 def Migration(y,time,nodes, direction):
@@ -44,19 +46,20 @@ def Birth(y, time, nodes):
     Return Value(s)
     --------------
     updated nodes
+    the new index
     """
     #len(nodes) will be the index of the new node, ie. the right child of y, the node giving birth
     #the region of the new node will have to be the same as the region of the parent node
     new_index=len(nodes)
     new_node=[time,y,nodes[y][2],[], -1]
     nodes.update({new_index:new_node})
-    return nodes
+    return (nodes, new_index)
 
 def Death(y,time,nodes):
     """
     Input Parameters
     ----------------
-    y           the index of the node giving dying
+    y           the index of the node giving dieing
     time        the time of death
     nodes       the node map from node index to
                [Time of Birth (0), Parent node (1), region (2), migration_times (3), Time of Death (4)]
@@ -64,7 +67,7 @@ def Death(y,time,nodes):
     --------------
     updated nodes
     """
-        nodes[y][4]=time;
+    nodes[y][4]=time;
         
     return nodes
 
@@ -79,8 +82,8 @@ def CreatePopulation(birth_0, death_0, birth_1, death_1, migration_1, migration_
     death_0         the per lineage death rate in state 0
     birth_1         the per lineage birth rate in state 1
     death_1         the per lineage death rate in state 1
-    migration_1     the effective migration rate from 0 to 1
-    migration_0     the effective migration rate from 1 to 0
+    migration_1     the per lineage migration rate from 0 to 1
+    migration_0     the per lineage migration rate from 1 to 0
     initial_0       the size of the initial population in state 0
     initial_1       the size of the initial population in state 1
     dt              the time step to be used
@@ -94,9 +97,114 @@ def CreatePopulation(birth_0, death_0, birth_1, death_1, migration_1, migration_
     final_0   the number of lineages in the boreal region at the tips of the tree (after all remaining lineages are related to eachother)
     final_1   the number of lineages in the tropical region at the tips of the tree (after all remaining are related to eachother)
     """
+    all_related=False;
+    time=0;
+    total_0=initial_0;
+    total_1=initial_1;
+    nodes={};
+    in_0=set(range(initial_0));
+    in_1=set(initial_0+range(initial_1));
+    for i in range(initial_0):
+        nodes.update({i : [0,-1,0,[],-1]});
+    for i in range(initial_1) :
+        nodes.update({i+initial_0 : [0,-1,1,[],-1]});
+    
+    while(all_related==False):
+        time=time+dt;
+        #prob_X is at least one X event in the time dt (1-Poisson probability no events), where X is birth_0, death_0
+        #birth_1, death_1, migration_0 or migration_1. Therefore, dt needs to be small enough
+        #such that the probability of two events is much smaller than the probability of one event in dt.
+        prob_birth_0=1-exp(-birth_0*total_0*dt);
+        prob_death_0=1-exp(-death_0*total_0*dt);
+        prob_birth_1=1-exp(-birth_1*total_1*dt);
+        prob_death_1=1-exp(-death_1*total_1*dt);
+        prob_migration_0=1-exp(-migration_0*total_0*dt);
+        prob_migration_1=1-exp(-migration_1*total_1*dt);
+        #Poisson distribution, probability of no events in time dt
+        prob_of_no_event=exp((-birth_0*dt+-death_0*dt+-migration_0*dt)*total_0+(-birth_1*dt+-death_1*dt+-migration_1*dt)*total_1);
+
+        #row_for_choosing 0:birth_0, 1:death_0, 2:birth_1, 3:death_1, 4:migration_0, 5:migratoin_1, 6:no event    
+        row_for_choosing=range(7);
+        row_for_choosing[0]=prob_birth_0;
+        row_for_choosing[1]=row_for_choosing[0]+prob_death_0;
+        row_for_choosing[2]=row_for_choosing[1]+prob_birth_1;
+        row_for_choosing[3]=row_for_choosing[2]+prob_death_1;
+        row_for_choosing[4]=row_for_choosing[3]+prob_migration_0;
+        row_for_choosing[5]=row_for_choosing[4]+prob_migration_1;
+        row_for_choosing[6]=row_for_choosing[5]+prob_of_no_event;
+        #normalize
+        row_for_choosing=row_for_choosing/row_for_choosing[6];
+        
+        event_number=Get_event(row_for_choosing);
+        in_0_copy=in_0.copy()
+        in_1_copy=in_1.copy()
+        all_lineages_copy=in_0_copy.union(in_1_copy);
+        
+        random_list=range(len(in_0_copy));
+        for i in in_0_copy:
+            random_list[i]=in_0_copy.pop();
+        random.shuffle(random_list);
+        random_index_0=random_list[0];
+        
+        random_list=range(len(in_1_copy));
+        for i in in_1_copy:
+            random_list[i]=in_1_copy.pop();
+        random.shuffle(random_list);
+        random_index_1=random_list[0];
+
+        random_list=range(len(all_lineages_copy  ))     
+        for i in all_lineages_copy:
+            random_list[i]=all_lineages_copy.pop();
+        random.shuffle(random_list);
+        random_index_all=random_list[0];
+        
+        if(event_number==0):#birth 0
+            index_to_birth=random_index_0;
+            (nodes, new_index)=Birth(index_to_birth,time,nodes);
+            in_0.update(new_index);
+        if(event_number==1):#death 0
+            index_to_die=random_index_0;
+            nodes=Death(index_to_die,time,nodes);
+            in_0.remove(index_to_die);
+        if(event_number==2):#birth 1
+            index_to_birth=random_index_1;
+            (nodes, new_index)=Birth(index_to_birth,time,nodes);
+            in_1.update(new_index);
+        if(event_number==3):#death 1
+            index_to_die=random_index_1;
+            nodes=Death(index_to_die,time,nodes);
+            in_1.remove(index_to_die);
+        if(event_number==4):#migration 0
+            index_to_migrate=random_index_all;
+            nodes=Migration(index_to_migrate,time,nodes,0)
+            if len(in_1.intersection(set([index_to_migrate])))!=0:
+                in_1.remove(index_to_migrate);
+                in_0.update(index_to_migrate);
+        if(event_number==4):#migration 1
+            index_to_migrate=random_index_all;
+            nodes=Migration(index_to_migrate,time,nodes,1)
+            if len(in_0.intersection(set([index_to_migrate])))!=0:
+                in_0.remove(index_to_migrate);
+                in_1.update(index_to_migrate);
+        
     return [nodes, final_0, final_1]
 
-def Simulate(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, intitial_0, initial_1, runs):
+def GetEvent(row_for_choosing):
+
+    n=random.uniform(0,1);
+    print(str(n)+"random")
+    for i in range(len(row_for_choosing)):
+        if(i==0):
+            if(0<=n<row_for_choosing[i]):
+                return (i)
+        if(i==(len(row_for_choosing)-1)):
+            if(row_for_choosing[i]<=n<=1):
+                return (i)
+        if(row_for_choosing[i-1]<=n<row_for_choosing[i]):
+                return (i)
+    return -1;            
+
+def Simulate(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, intitial_0, initial_1):
     """
     Input Parameters
     ----------------
@@ -104,12 +212,11 @@ def Simulate(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, i
     death_0         the per lineage death rate in state 0
     birth_1         the per lineage birth rate in state 1
     death_1         the per lineage death rate in state 1
-    migration_1     the effective migration rate from 0 to 1
-    migration_0     the effective migration rate from 1 to 0
+    migration_1     the per lineage migration rate from 0 to 1
+    migration_0     the per lineage migration rate from 1 to 0
     initial_0       the size of the initial population in state 0
     initial_1       the size of the initial population in state 1
     dt              the time step to be used
-    runs            the number of times to run the burn in simulation
         
     Return Values
     -------------
@@ -128,5 +235,5 @@ def Simulate(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, i
     #not known, so it will be best to at first have the birth rate only slightly smaller than the death rate,
     #and have run equal 2 or 3.
     for i in range(0,runs):
-        [nodes,initial_0,initial_1]=CreatePopulation(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, intitial_0, initial_1, runs)
+        [nodes,initial_0,initial_1]=CreatePopulation(birth_0, death_0, birth_1, death_1, migration_1, migration_0, dt, intitial_0, initial_1)
     return nodes
